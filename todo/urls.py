dca1f5cb5ab95c504a3927820400912889db8111
Jaskaran Singh <er.jkjaskaran@gmail.com>
2022-05-20 20:02:34 +0530

    Todo Create and View all TODOs

--- todo/urls.py
+++ todo/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns=[
-    path('', views.index, name="index"),
+    path('', views.index, name="home"),
+    path('create_todo', views.create_todo, name="create-todo"),
+    path('todo/<id>', views.todo_details, name="todo")
 ]
--- todo/views.py
+++ todo/views.py
@@ -1,6 +1,30 @@
 from django.shortcuts import render
+from .forms import TodoForm
+from .models import Todo
+from django.http import HttpResponseRedirect
+from django.urls import reverse
 
 
 # Create your views here.
 def index(request):
-    return render(request, 'todo/index.html')
+    todo = Todo.objects.all()
+    context = {'todos': todo}
+    return render(request, 'todo/index.html', context)
+
+
+def create_todo(request):
+    if request.method == "POST":
+        todo = Todo()
+        todo.title = request.POST.get("title")
+        todo.description = request.POST.get("description")
+        todo.is_completed = True if request.POST.get("is_completed", False) == 'on' else False
+        todo.save()
+        return HttpResponseRedirect(reverse("todo", kwargs={'id': todo.pk}))
+    form = TodoForm()
+    context = {'form': form}
+    return render(request, 'todo/create_todo.html', context)
+
+
+def todo_details(request, id):
+
+    return render(request, 'todo/todo-details.html', {})
